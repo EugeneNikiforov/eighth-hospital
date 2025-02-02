432c72d25f5346ed02cc06e3a139767205c3f966
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2025-02-02 21:34:25 +0200

    add navitem module

--- src/components/NavPanel/NavItem/NavItem.jsx
+++ src/components/NavPanel/NavItem/NavItem.jsx
@@ -1,8 +1,17 @@
+import ccs from "./nav-item.module.scss";
+
 const NavItem = ({ navitem }) => {
   const { name } = navitem;
   return (
-    <li>
+    <li className={ccs.navigationItem}>
       <p>{name}</p>
+      <div>
+        <a href="@">Наша історія</a>
+        <a href="@">Звітність</a>
+        <a href="@">Досягнення</a>
+        <a href="@">Благодійність</a>
+        <a href="@">Залишки препаратів</a>
+      </div>
     </li>
   );
 };
